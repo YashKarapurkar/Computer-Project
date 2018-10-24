2f0e2091a439e632383ede0e8083bf681ff89372
Atharv Sapre <32292355+Atharvsam@users.noreply.github.com>
2018-10-24 22:08:15 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,53 +2,19 @@
 #include<fstream>
 #include<string.h>
 #include<stdlib.h>
-#include "login.cpp"
+#include "login.cpp" // Has user credentials access class and functions
+#include "shop_list.cpp" // Has the car class
 
 using namespace std;
 
-class car
-{
-public:
-    int index;
-	char modelName[30], engine[10];
-    int horsePower;
-	float cost;
 
-    void get_data()
-    {
-        cout<<"Index No: ";
-        cin>>index;
-        cout<<"Model Name: ";
-        cin>>modelName;
-        cout<<"Engine: ";
-        cin>>engine;
-        cout<<"Horse Power: ";
-        cin>>horsePower;
-        cout<<"Cost(In Dollars): ";
-        cin>>cost;
-    }
-
-    void display()
-    {
-        cout<<"Index No: "<<index<<endl;
-        cout<<"Model Name: "<<modelName<<endl;
-        cout<<"Engine: "<<engine<<endl;
-        cout<<"Horse Power: "<<horsePower<<endl;
-        cout<<"Cost: "<<cost<<" Million $"<<endl<<endl;
-    }
-
-    int indx()
-    {
-        return index;
-    }
-};
 
 int main()
 {
 
     void main_menu();
 
-    char username[20], password[20];
+    char username[20], password[20], ch='y';
     ifstream fin("secure.dat", ios::in|ios::binary);
     ofstream fout("cars.DB", ios::app|ios::binary);
     int dec, ret, ext = 0;
@@ -59,12 +25,12 @@ int main()
         ext = 0;
         system("pause");
         system("clear");
-        cout<<"<<Welcome to Auto-Expo 2018>>\n";
-        cout<<"1. Have an account? - LOG-IN\n";
-        cout<<"2. Join Us! - SIGN-UP\n";
-        cout<<"3. Add Cars\n";
-        cout<<"4. Exit\n";
-        cout<<"Input: \n";
+        cout<<"\t\t<<Welcome to Auto-Expo 2018>>\n";
+        cout<<"\t\t1. Have an account? - LOG-IN\n";
+        cout<<"\t\t2. Join Us! - SIGN-UP\n";
+        cout<<"\t\t3. Add Cars\n";
+        cout<<"\t\t4. Exit\n";
+        cout<<"\t\tInput: \n";
         cin>>dec;
         //system("pause");
 
@@ -72,10 +38,10 @@ int main()
         {
             case 1:
             //system("clear");
-            cout<<"<<Log - In>>\n";
-            cout<<"Username: ";
+            cout<<"\t\t<<Log - In>>\n";
+            cout<<"\t\tUsername: ";
             cin>>username;
-            cout<<"Password: ";
+            cout<<"\t\tPassword: ";
             cin>>password;
 
             ret = verify(username, password);
@@ -86,7 +52,7 @@ int main()
             }
             else if(ret == 1)
             {
-                cout<<"Wrong Credentials!\n";
+                cout<<"\t\tWrong Credentials!\n";
                 //system("pause");
                 //system("clear");
             }
@@ -97,9 +63,17 @@ int main()
             break;
 
             case 3:
-            cout<<"<<Car Adder Menu>>\n";
+            file:
+            system("clear");
+            cout<<"\t\t<<Car Adder Menu>>\n";
             car_adder.get_data();
             fout.write((char*)&car_adder, sizeof(car_adder));
+            cout<<"\t\tDo you want to add more: ";
+            cin>>ch;
+            if(ch == 'y' || ch == 'Y')
+            {
+                goto file;
+            }
             fout.close();
 
             case 4:
@@ -107,7 +81,7 @@ int main()
             break;
 
             default:
-            cout<<"Wrong option!\n";
+            cout<<"\t\tWrong option!\n";
         }
 
         if(ext == 1)
@@ -126,21 +100,22 @@ void main_menu()
         ext = 0;
         system("pause");
         system("clear");
-        cout<<"<<Main Menu>>\n";
-        cout<<"1. Shop\n";
-        cout<<"2. Your Orders\n";
-        cout<<"3. Exit\n";
-        cout<<"Input: ";
+        cout<<"\t\t<<Main Menu>>\n";
+        cout<<"\t\t1. Shop\n";
+        cout<<"\t\t2. Your Orders\n";
+        cout<<"\t\t3. Exit\n";
+        cout<<"\t\tInput: ";
         cin>>dec;
 
         switch(dec)
         {
             case 1:
-            cout<<"Shop";
+            cout<<"\t\t<<Shop>>\n";
+
             break;
 
             case 2:
-            cout<<"Orders";
+            cout<<"\t\tOrders";
             break;
 
             case 3:
@@ -148,7 +123,7 @@ void main_menu()
             break;
 
             default:
-            cout<<"Wrong Option!\n";
+            cout<<"\t\tWrong Option!\n";
         }
 
         if(ext == 1)
